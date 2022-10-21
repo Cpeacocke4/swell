38050c6403ba19f5271df6c7263e299538964ede
Christy Peacocke <christypeacocke@Christys-MacBook-Pro.local>
2022-10-21 16:19:55 +0200

    Add location and forecast API request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Forecast from "./components/Forecast";
 
 const App = () => {
+    const [location, setLocation] = useState({lat: null, lng: null});
+
+    useEffect(() => {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => setLocation({lat: position.coords.latitude, lng: position.coords.longitude})
+        );
+    
+    }, []);
+
+    console.log(location);  
+    
     return (
-        <Forecast />
+        <Forecast lat={location.lat} lng={location.lng}/>
     )
 };
 
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,38 +1,46 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 
-const Forecast = () => {
+const Forecast = ({lat, lng}) => {
     const [forecast, setForecast] = useState({
-        sky: {},
-        temp: {},
-        wind: {},
-        swell: {},
+        result: {
+            sky: [],
+            temp: [],
+            wind: [],
+            swell: [],
+    }
     });
 
     const params = 'airTemperature,cloudCover,gust,swellHeight';
     
     const today = new Date();
-    const time = today.getHours() + '.' + today.getMinutes();
+    const time = Math.floor(today.getTime() / 1000);
     console.log(time);
 
     useEffect(() => {
-        const lat = window.navigator.geolocation.getCurrentPosition.coords.latitude
-        console.log(lat);
+        if (!lat) return
+        if (!lng) return
 
         const getForecast = async () => {
-            const {data} = await axios.get(`https://api.stormglass.io/v2/weather/point?lat=${location.lat}&lng=${location.long}&params=${params}`, {
+            const {data} = await axios.get(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}&start=${time}`, {
                 headers: {
                     'Authorization' : 'bbec2bc0-4e1f-11ed-bce5-0242ac130002-bbec2c2e-4e1f-11ed-bce5-0242ac130002'
                 },
             });
-            setForecast(...forecast, ...data.hours[0])
-            console.log(forecast);
+            console.log(data.hours);
+            setForecast({...forecast, result: {
+                sky: data.hours[0].cloudCover.noaa,
+                temp: data.hours[0].airTemperature.noaa,
+                wind: data.hours[0].gust.noaa,
+                swell: data.hours[0].swellHeight.noaa,
+            }});
         };
-
-        getForecast();
-    }, []);
     
-    return <div>Your position is Lat: {}, Long: {}</div>
+        getForecast();
+    
+    }, [lat,lng])
+
+    return <div> Cloud cover: {forecast.result.sky} temp: {forecast.result.temp} wind: {forecast.result.wind} swell: {forecast.result.swell}</div>
 };
 
 export default Forecast;
